4ab1afd63cc747d45e8b601569801ea9ce736fdb
Victor Uma <umavictor11@gmail.com>
2024-03-12 03:02:38 +0100

    added new test

--- cypress/e2e/getTextVar.spec.js
+++ cypress/e2e/getTextVar.spec.js
@@ -13,3 +13,20 @@ describe("template spec", () => {
     cy.log("print value of h3", getText);
   });
 });
+
+describe("Shop Menu Navigation", () => {
+  it("successfully navigates to Accessories from Shop menu", () => {
+    // Step 1: Visit the Ecommerce Playground
+    cy.visit("https://ecommerce-playground.lambdatest.io/");
+
+    // Step 2: Hover over the "Mega menu" dropdown
+
+    cy.get("li").contains("Menu").invoke("show");
+
+    // Step 3: Click on the "Apple" category within the dropdown
+    cy.get("ul.mega-menu-content").contains("Apple").click({ force: true });
+
+    // Optional: Assert that the URL is correct after clicking "Apple"
+    cy.url().should("include", "product/manufacturer");
+  });
+});
